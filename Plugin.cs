cc308fdaed02832aa37d24b6b8c0c0c99ecb1ba0
Witchybun <embenham05@gmail.com>
2024-10-17 22:16:59 -0500

    Baby Start

---
+++ Plugin.cs
@@ -0,0 +1,32 @@
+﻿﻿using BepInEx;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using HarmonyLib;
+using Newtonsoft.Json;
+using System.IO;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+
+
+namespace FrogDetectiveAP 
+{
+
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public class Plugin : BaseUnityPlugin 
+    {
+        public const string PluginGUID = "com.Albrekka.FrogDetectiveAP";
+        public const string PluginName = "FrogDetectiveAP";
+        public const string PluginVersion = "0.0.1";
+        public static string SettingsPath = Application.persistentDataPath + "/Randomizer/Settings.json";
+        public static string ItemTrackerPath = Application.persistentDataPath + "/Randomizer/ItemTracker.json";
+        public static string SpoilerLogPath = Application.persistentDataPath + "/Randomizer/Spoiler.log";
+        public ManualLogSource FroggyLogger;
+
+        public void Awake()
+        {
+            FroggyLogger = new ManualLogSource("FrogDetectiveAP");
+            FroggyLogger.LogInfo("Ribbit!  We're live!");
+        }
+    }
+}
+            
